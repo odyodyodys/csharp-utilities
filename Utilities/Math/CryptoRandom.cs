ec13497579dfa3092861082e6a3377061d6f3893
odys <odyodyodys@gmail.com>
2013-07-07 15:45:15 +0000

    Added CryptoRandom

---
+++ Utilities/Math/CryptoRandom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Randomizer
+{
+    internal class CryptoRandom
+    {
+        internal static int Next(int max)
+        {
+            return Next(0, max);
+        }
+
+        internal static int Next(int min, int max)
+        {
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] buffer = new byte[4];
+
+            rng.GetBytes(buffer);
+            int result = BitConverter.ToInt32(buffer, 0);
+
+            return new Random(result).Next(min, max);
+        }
+    }
+}
