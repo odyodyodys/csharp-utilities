c45c0e87eb60a0c7e0021f192801e7676ff794fc
odys <odyodyodys@gmail.com>
2013-10-07 19:31:49 +0000

    Added NlogConfigurator, Changed target framework to 4.0 (from 3.5)

---
+++ Utilities/ThirdParty/NlogConfigurator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog.Config;
+using NLog.Targets;
+using NLog;
+
+namespace Utilities.ThirdParty
+{
+    internal static class NlogConfigurator
+    {
+        internal static void ConfigureLogger(string mainApplicationName, string baseFilename)
+        {
+            LoggingConfiguration nlogConfig = new LoggingConfiguration();
+            FileTarget target = new FileTarget();
+            nlogConfig.AddTarget(baseFilename, target);
+
+            // configure target
+            target.Layout = @"${date:format=HH\:mm\:ss.ff} Th${threadid} ${level} ${logger} ${message} ${exception:format=tostring}";
+            target.FileName = @"${specialfolder:folder=ApplicationData}/" + mainApplicationName + "/logs/" + baseFilename + ".log";
+            target.ArchiveFileName = @"${specialfolder:folder=ApplicationData}/" + mainApplicationName + "/logs/" + baseFilename + ".{#}.log";
+            target.ArchiveAboveSize = 4194304; // 4Mb
+            target.ArchiveNumbering = ArchiveNumberingMode.Sequence;
+            target.ConcurrentWrites = true;
+            target.MaxArchiveFiles = 10;
+            target.KeepFileOpen = false;
+            target.Encoding = Encoding.UTF8;
+
+            // define rules
+            LoggingRule rule = new LoggingRule("*", LogLevel.Debug, target);
+            nlogConfig.LoggingRules.Add(rule);
+
+            LogManager.Configuration = nlogConfig;
+        }
+    }
+}
