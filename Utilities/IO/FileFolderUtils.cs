70075a92ea4ed7d93714162d1934e30a0d9cc0b6
odys <odyodyodys@gmail.com>
2015-05-14 16:52:34 +0000

    Added FileFolderUtils and NaturalStringComparer

---
+++ Utilities/IO/FileFolderUtils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Utilities.IO
+{
+    public static class FileFolderUtils
+    {
+        public static void CopyFolderRecursively(string SourcePath, string DestinationPath)
+        {
+            // ensure destination folder exists
+            Directory.CreateDirectory(DestinationPath);
+
+            foreach (string dirPath in Directory.GetDirectories(SourcePath, "*", SearchOption.AllDirectories))
+            {
+                Directory.CreateDirectory(dirPath.Replace(SourcePath, DestinationPath));
+            }
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles(SourcePath, "*.*", SearchOption.AllDirectories))
+            {
+                File.Copy(newPath, newPath.Replace(SourcePath, DestinationPath), true);
+            }
+        }
+
+        public static void EmptyFolder(string path)
+        {
+            System.IO.DirectoryInfo downloadedMessageInfo = new DirectoryInfo(path);
+
+            foreach (FileInfo file in downloadedMessageInfo.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in downloadedMessageInfo.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+        }
+    }
+}
---
+++ Utilities/StringUtils/NaturalStringComparer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security;
+using System.Runtime.InteropServices;
+
+namespace Utilities.StringUtils
+{
+    //http://stackoverflow.com/a/248613/245495
+
+    [SuppressUnmanagedCodeSecurity]
+    internal static class SafeNativeMethods
+    {
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+        public static extern int StrCmpLogicalW(string psz1, string psz2);
+    }
+
+    public sealed class NaturalStringComparer : IComparer<string>
+    {
+        public int Compare(string a, string b)
+        {
+            return SafeNativeMethods.StrCmpLogicalW(a, b);
+        }
+    }
+}
